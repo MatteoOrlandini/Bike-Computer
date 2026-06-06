22fd1d0d384305f34f0c91df1a5687b73b663ba6
Giulia Latini <matteoorlandini.mail@gmail.com>
2026-06-06 18:04:49 +0200

    Added Nimble BLE

--- examples/ESP-IDF/08_lvgl_Porting/main/main.c
+++ examples/ESP-IDF/08_lvgl_Porting/main/main.c
@@ -3,28 +3,89 @@
  *
  * SPDX-License-Identifier: CC0-1.0
  */
-
 #include "waveshare_rgb_lcd_port.h"
+#include "nvs_flash.h"
+/* BLE */
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
+#include "host/ble_hs.h"
+#include "host/util/util.h"
+#include "services/gap/ble_svc_gap.h"
+
+// static const char *TAG = "bike_computer";
+static const char *BLE_DEVICE_NAME = "BikeComputer";
+
+static void ble_advertise(void);
+
+static void ble_on_sync(void)
+{
+    ble_addr_t addr;
+    ble_hs_id_gen_rnd(1, &addr);
+    ble_hs_id_set_rnd(addr.val);
+    ble_advertise();
+}
+
+static void ble_on_reset(int reason)
+{
+    ESP_LOGE(TAG, "BLE reset, reason=%d", reason);
+}
+
+static void ble_advertise(void)
+{
+    struct ble_gap_adv_params adv_params = {0};
+    struct ble_hs_adv_fields fields = {0};
+
+    fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_BREDR_UNSUP;
+    fields.name = (uint8_t *)BLE_DEVICE_NAME;
+    fields.name_len = strlen(BLE_DEVICE_NAME);
+    fields.name_is_complete = 1;
+    ble_gap_adv_set_fields(&fields);
+
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND;
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    int rc = ble_gap_adv_start(BLE_OWN_ADDR_RANDOM, NULL, BLE_HS_FOREVER,
+                               &adv_params, NULL, NULL);
+    if (rc != 0) {
+        ESP_LOGE(TAG, "BLE adv start failed: %d", rc);
+    } else {
+        ESP_LOGI(TAG, "BLE advertising as \"%s\"", BLE_DEVICE_NAME);
+    }
+}
+
+static void nimble_host_task(void *param)
+{
+    nimble_port_run();
+    nimble_port_freertos_deinit();
+}
 
 void app_main()
 {
-    waveshare_esp32_s3_rgb_lcd_init(); // Initialize the Waveshare ESP32-S3 RGB LCD 
-    // wavesahre_rgb_lcd_bl_on();  //Turn on the screen backlight 
-    // wavesahre_rgb_lcd_bl_off(); //Turn off the screen backlight 
-    
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ret = nimble_port_init();
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to init nimble: %d", ret);
+        return;
+    }
+    ble_hs_cfg.sync_cb  = ble_on_sync;
+    ble_hs_cfg.reset_cb = ble_on_reset;
+    ble_svc_gap_device_name_set(BLE_DEVICE_NAME);
+    nimble_port_freertos_init(nimble_host_task);
+
+    waveshare_esp32_s3_rgb_lcd_init();
+
     ESP_LOGI(TAG, "Display LVGL demos");
-    // Lock the mutex due to the LVGL APIs are not thread-safe
     if (lvgl_port_lock(-1)) {
-        // lv_demo_stress();
-        // lv_demo_benchmark();
-        // lv_demo_music();
 #if CONFIG_EXAMPLE_LCD_TOUCH_CONTROLLER_GT911
         lv_demo_widgets();
 #else
         lv_demo_music();
 #endif
-        // example_lvgl_demo_ui();
-        // Release the mutex
         lvgl_port_unlock();
     }
 }
