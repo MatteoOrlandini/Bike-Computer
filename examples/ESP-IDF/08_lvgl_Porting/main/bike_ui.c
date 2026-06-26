522a98787d0024b8dcfb4f77fce67cbfdde29912
Giulia Latini <matteoorlandini.mail@gmail.com>
2026-06-26 19:37:41 +0200

    Increased label size

--- examples/ESP-IDF/08_lvgl_Porting/main/bike_ui.c
+++ examples/ESP-IDF/08_lvgl_Porting/main/bike_ui.c
@@ -42,83 +42,90 @@ void bike_ui_init(trip_data_t *data)
 
     // --- Current speed (large, top center) ---
     lv_obj_t *lbl_spd_title = lv_label_create(scr);
+    lv_obj_set_style_text_font(lbl_spd_title, &lv_font_montserrat_36, 0);
     lv_label_set_text(lbl_spd_title, "km/h");
     lv_obj_set_style_text_color(lbl_spd_title, lv_color_hex(0xAAAAAA), 0);
     lv_obj_align(lbl_spd_title, LV_ALIGN_TOP_MID, 0, 10);
 
     label_speed = lv_label_create(scr);
-    lv_obj_set_style_text_font(label_speed, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(label_speed, &lv_font_montserrat_36, 0);
     lv_obj_set_style_text_color(label_speed, lv_color_hex(0x00FF00), 0);
     lv_label_set_text(label_speed, "--.-");
-    lv_obj_align(label_speed, LV_ALIGN_TOP_MID, 0, 30);
+    lv_obj_align(label_speed, LV_ALIGN_TOP_MID, 0, 50);
 
     // --- Elapsed time ---
     lv_obj_t *lbl_time_title = lv_label_create(scr);
+    lv_obj_set_style_text_font(lbl_time_title, &lv_font_montserrat_36, 0);
     lv_label_set_text(lbl_time_title, "Time");
     lv_obj_set_style_text_color(lbl_time_title, lv_color_hex(0xAAAAAA), 0);
     lv_obj_align(lbl_time_title, LV_ALIGN_TOP_LEFT, 20, 140);
 
     label_time = lv_label_create(scr);
-    lv_obj_set_style_text_font(label_time, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(label_time, &lv_font_montserrat_36, 0);
     lv_obj_set_style_text_color(label_time, lv_color_white(), 0);
     lv_label_set_text(label_time, "00:00:00");
-    lv_obj_align(label_time, LV_ALIGN_TOP_LEFT, 20, 160);
+    lv_obj_align(label_time, LV_ALIGN_TOP_LEFT, 20, 180);
 
     // --- Avg speed ---
     lv_obj_t *lbl_avg_title = lv_label_create(scr);
+    lv_obj_set_style_text_font(lbl_avg_title, &lv_font_montserrat_36, 0);
     lv_label_set_text(lbl_avg_title, "Avg km/h");
     lv_obj_set_style_text_color(lbl_avg_title, lv_color_hex(0xAAAAAA), 0);
     lv_obj_align(lbl_avg_title, LV_ALIGN_TOP_RIGHT, -20, 140);
 
     label_avg = lv_label_create(scr);
-    lv_obj_set_style_text_font(label_avg, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(label_avg, &lv_font_montserrat_36, 0);
     lv_obj_set_style_text_color(label_avg, lv_color_white(), 0);
     lv_label_set_text(label_avg, "--.-");
-    lv_obj_align(label_avg, LV_ALIGN_TOP_RIGHT, -20, 160);
+    lv_obj_align(label_avg, LV_ALIGN_TOP_RIGHT, -20, 180);
 
     // --- Total km ---
     lv_obj_t *lbl_total_title = lv_label_create(scr);
+    lv_obj_set_style_text_font(lbl_total_title, &lv_font_montserrat_36, 0);
     lv_label_set_text(lbl_total_title, "Total km");
     lv_obj_set_style_text_color(lbl_total_title, lv_color_hex(0xAAAAAA), 0);
     lv_obj_align(lbl_total_title, LV_ALIGN_TOP_LEFT, 20, 260);
 
     label_total = lv_label_create(scr);
-    lv_obj_set_style_text_font(label_total, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(label_total, &lv_font_montserrat_36, 0);
     lv_obj_set_style_text_color(label_total, lv_color_white(), 0);
     lv_label_set_text(label_total, "0.000");
-    lv_obj_align(label_total, LV_ALIGN_TOP_LEFT, 20, 280);
+    lv_obj_align(label_total, LV_ALIGN_TOP_LEFT, 20, 300);
 
     // --- Gradient ---
     lv_obj_t *lbl_grad_title = lv_label_create(scr);
+    lv_obj_set_style_text_font(lbl_grad_title, &lv_font_montserrat_36, 0);
     lv_label_set_text(lbl_grad_title, "Gradient %");
     lv_obj_set_style_text_color(lbl_grad_title, lv_color_hex(0xAAAAAA), 0);
     lv_obj_align(lbl_grad_title, LV_ALIGN_TOP_MID, 0, 260);
 
     label_gradient = lv_label_create(scr);
-    lv_obj_set_style_text_font(label_gradient, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(label_gradient, &lv_font_montserrat_36, 0);
     lv_obj_set_style_text_color(label_gradient, lv_color_white(), 0);
     lv_label_set_text(label_gradient, "0.0");
-    lv_obj_align(label_gradient, LV_ALIGN_TOP_MID, 0, 280);
+    lv_obj_align(label_gradient, LV_ALIGN_TOP_MID, 0, 300);
 
     // --- Ascent ---
     lv_obj_t *lbl_ascent_title = lv_label_create(scr);
+    lv_obj_set_style_text_font(lbl_ascent_title, &lv_font_montserrat_36, 0);
     lv_label_set_text(lbl_ascent_title, "Ascent m");
     lv_obj_set_style_text_color(lbl_ascent_title, lv_color_hex(0xAAAAAA), 0);
     lv_obj_align(lbl_ascent_title, LV_ALIGN_TOP_RIGHT, -20, 260);
 
     label_ascent = lv_label_create(scr);
-    lv_obj_set_style_text_font(label_ascent, &lv_font_montserrat_24, 0);
+    lv_obj_set_style_text_font(label_ascent, &lv_font_montserrat_36, 0);
     lv_obj_set_style_text_color(label_ascent, lv_color_white(), 0);
     lv_label_set_text(label_ascent, "0");
-    lv_obj_align(label_ascent, LV_ALIGN_TOP_RIGHT, -20, 280);
+    lv_obj_align(label_ascent, LV_ALIGN_TOP_RIGHT, -20, 300);
 
     // --- Reset button (bottom center) ---
     lv_obj_t *btn = lv_btn_create(scr);
-    lv_obj_set_size(btn, 120, 40);
+    lv_obj_set_size(btn, 160, 60);
     lv_obj_align(btn, LV_ALIGN_BOTTOM_MID, 0, -10);
     lv_obj_add_event_cb(btn, reset_btn_cb, LV_EVENT_CLICKED, NULL);
 
     lv_obj_t *btn_label = lv_label_create(btn);
+    lv_obj_set_style_text_font(btn_label, &lv_font_montserrat_36, 0);
     lv_label_set_text(btn_label, "Reset");
     lv_obj_center(btn_label);
 
